ad9c0b5cecc754fcb74e4757640836f5a11ea0a3
Zaidi <zaidiahsan.r@gmail.com>
2023-03-28 17:13:10 -0500

    tokens verification middleware

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -6,16 +6,17 @@ import {
     getHotels, 
     updateHotel 
 } from '../controllers/hotel.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 /** CRUD Operations for hotels */
 //Create
-router.post("/", createHotel);
+router.post("/", verifyAdmin, createHotel);
 //Update
-router.put("/:id", updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 //Delete 
-router.delete("/:id", deleteHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 //Get
 router.get("/:id", getHotel);
 //Get All
